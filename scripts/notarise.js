232fd3185c82f167314562dace9d1499ebbcd79c
Tomas Triska <tomas.triska@icloud.com>
2018-03-30 15:54:29 +0200

    Font size change

--- scripts/notarise.js
+++ scripts/notarise.js
@@ -1,3 +1,19 @@
-$("upload-button-notarise").click(function () { 
-    
+var provider;
+
+window.addEventListener('load', function () {
+
+    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+    if (typeof web3 !== 'undefined') {
+
+        // Use the browser's ethereum provider
+        provider = web3.currentProvider
+
+    } else {
+        console.log('No Etherum accound found. Please log into MetaMask or Mist')
+    }
+
+})
+
+$("#upload-button-notarise").click(function () {
+    alert(provider);
 });
