456ff528a29a7d69521266805c9a5d7d6a7dd3b8
Jan Paricka <jparicka@gmail.com>
2018-05-10 20:50:47 +0200

    cleaned up urlpatterns

--- notary/urls.py
+++ notary/urls.py
@@ -13,16 +13,4 @@ urlpatterns = [
     url(r'^login/$', auth, name='login'),
     url(r'^register/$', register, name='login'),
     url(r'^logout/$', user_logout, name='logout'),
-    #url(r'^account/settings/$', 'userprofile.views.account_settings', name='account_settings'),
-    #url(r'^account/password/$', 'userprofile.views.change_password', name='change_password'),
 ]
-
-#urlpatterns += patterns('',
-#    # static and media files
-#    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
-#        'document_root': settings.STATIC_ROOT,
-#    }),
-#    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-#        'document_root': settings.MEDIA_ROOT, 'show_indexes': False,
-#    }),
-#)
