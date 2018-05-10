7226b233c5fca075e3f1cd7f0c8f6bffdb0b2144
Jan Paricka <jparicka@gmail.com>
2018-05-10 17:42:41 +0200

    added on static files onto the django project

--- temp-django/notary/settings.py
+++ temp-django/notary/settings.py
@@ -20,23 +20,27 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&z7lwkrzvx&c%-d(f+6&%e0_oy0lq6!0((rw@^80gnys^rvmgm'
+SECRET_KEY = '&z7lwkrzvx&c%-d(f+6&%e0_oy0-jparicka-lq6!0((rw@^80gnys^rvmgm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    # global templatetags
+    'notary',
+    # views
 ]
 
 MIDDLEWARE = [
@@ -115,6 +119,18 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..', 'static'))
 STATIC_URL = '/static/'
+
+LOGIN_URL = '/login/'
+LOGOUT_URL = '/logout/'
+
+STATICFILES_DIRS = (
+    os.path.abspath(os.path.join(BASE_DIR, 'static')),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
