1607435f89f46fd9f2e68cac97ceb911ea5bb5d9
Jan Paricka <jparicka@gmail.com>
2018-05-10 20:20:31 +0200

    setting cleansing

--- notary/settings.py
+++ notary/settings.py
@@ -1,19 +1,12 @@
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '&z7lwkrzvx&c%-d(f+6&%e0_oy0-paricka-lq6!0((rw@^80gnys^rvmgm'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 INSTALLED_APPS = [
@@ -97,13 +90,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
