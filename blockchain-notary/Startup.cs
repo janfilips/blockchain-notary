5cd1e1ea6e810b414cd16150fa1ff02b1d2a840b
Iver88 <tomas.triska@icloud.com>
2018-05-10 13:07:16 +0200

    MySQL support

--- blockchain-notary/Startup.cs
+++ blockchain-notary/Startup.cs
@@ -29,7 +29,7 @@ namespace blockchain_notary
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
