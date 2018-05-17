d30ce1c6d78c0f22000a091eaeb2fe2c54531a05
Jan Paricka <jparicka@gmail.com>
2018-05-17 10:52:59 +0200

    added on notary contract

--- contracts/notary.sol
+++ contracts/notary.sol
@@ -1,1 +1,23 @@
+pragma solidity ^0.4.13;
 
+
+contract Notarise {
+
+  mapping (bytes32 => bytes32) private proofs;
+
+  // store a proof of existence in the contract state
+  // *transactional function*
+  function notariseWithTitle(bytes32 proof, bytes32 document_title) public {
+    proofs[proof] = document_title;
+  }
+  function notarise(bytes32 proof) public {
+    proofs[proof] = "notarised";
+  }
+
+  // returns true if proof is stored
+  // *read-only function*
+  function hasProof(bytes32 proof) public constant returns (bytes32) {
+    return proofs[proof];
+  }
+
+}
