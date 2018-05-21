28be39d72bbbe1204fb7316c6d8e3ead39b37f13
Tomáš Tříska <tomas.triska@icloud.com>
2018-05-21 11:30:11 +0200

    Notifications system

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,4 +1,20 @@
 // Shows modal dialog
 function showAlert(text, title = "Alert", buttonText = "OK") {
-    alert(text);
+    $("#modal .modal-title").text(title);
+    $("#modal .modal-body p").html(text);
+    $("#modal-button").text(buttonText);
+    $("#modal").show();
 }
+
+function modalClose() {
+    $("#modal").hide();
+}
+
+$(document).ready(function () {
+    $("#modal .close").click(function () {
+        modalClose();
+    });
+    $("#modal-button").click(function () {
+        modalClose();
+    });
+});
