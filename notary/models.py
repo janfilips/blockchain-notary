47c7db581f26f153b400531c30c51aa74dff00ce
Jan Paricka <jparicka@gmail.com>
2018-05-23 13:55:20 +0200

    cleaned up models

--- notary/models.py
+++ notary/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.conf import settings
-
 
 class Submissions(models.Model):
     file_name=models.CharField(max_length=255)
