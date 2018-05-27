c991b462d434c6f58aa96f6c586c0e9224085c31
Jan Paricka <jparicka@gmail.com>
2018-05-27 16:42:54 +0200

    introduced persistent contract storage for proofs

--- contracts/notary.sol
+++ contracts/notary.sol
@@ -1,39 +1,38 @@
 pragma solidity ^0.4.13;
 
+
 contract Notary {
     
     address public investor1 = 0x96E0089c04c99E69E4445787bA4CC3cEA6e1B82f;
     address public investor2 = 0xCF4e87991826081d172B61b2e1B2800F18dA8cE7;
 
-    // TODO XXX: replace this with storage, this needs to be accessible from newly deployed contracts
-    // right now, when you deploy a new contract, all this information is being lost as it is stored in an in-memory per-contract base.
-    mapping (bytes32 => bool) private proofs;
+    address NotaryPersistentStorage = 0x452298c6651b73886e5305f66c717a68cc7047c9;
 
-    event LogBalance(address, uint);
-    
+    event LogResponse(bytes32, bool);
+
     function Notary() payable {
     }
     
-    function notarise(bytes32 proof) public payable returns (bool success) {
-        proofs[proof] = true;
+    function notarise(bytes32 _proof) public payable returns (bool success) {
+        
+        bool response = NotaryPersistentStorage.call(bytes4(sha3("storeProof(bytes32)")),_proof);
+        if(response==false) {
+            throw;
+        }
+        
         _payRoyalty();
+        
         return true;
-    }
-
-    function _payRoyalty() public payable {
-        uint amount = msg.value;
-        investor1.transfer(amount/10*9);
-        investor2.transfer(amount/10);
     }
     
-    function hasProof(bytes32 proof) public constant returns (bool) {
-        return proofs[proof];
-        return true;
+    function _payRoyalty() public payable {
+        uint amount = msg.value;
+        investor1.transfer(amount/100*99);
+        investor2.transfer(amount/100);
     }
-
+    
     // fallback function
     function () payable {
     }
     
 }
-
