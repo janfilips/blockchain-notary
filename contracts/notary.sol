53deff19da577060b0bbe0f0352445bb54044360
Jan Paricka <jparicka@gmail.com>
2018-05-27 21:28:16 +0200

    trying out the new contract

--- contracts/notary.sol
+++ contracts/notary.sol
@@ -6,7 +6,7 @@ contract Notary {
     address public investor1 = 0x96E0089c04c99E69E4445787bA4CC3cEA6e1B82f;
     address public investor2 = 0xCF4e87991826081d172B61b2e1B2800F18dA8cE7;
 
-    address NotaryPersistentStorageAddress = 0x2137bce6afca3c582c188248d48bb1ceb2cd43a1;
+    address NotaryPersistentStorageAddress = 0x3163b3d71083fcf3fc0c031f52c00f9ea18f2452;
 
     event LogResponse(bytes32, bool);
 
@@ -26,6 +26,7 @@ contract Notary {
     function hasProof(bytes32 _proof) public returns (bool) {
         NotaryPersistentStorage notary = NotaryPersistentStorage(NotaryPersistentStorageAddress);
         bool result = notary.hasProof(_proof);
+        LogResponse(_proof,result);
         return result;
     }
     
