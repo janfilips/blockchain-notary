374cf4e0c9abda20dd9cbe6e1f43a6ba99daeb66
Jan Paricka <jparicka@gmail.com>
2018-05-31 11:37:03 +0000

    added on generic gunicorn start script

--- gunicorn.conf.py
+++
@@ -1,5 +0,0 @@
-bind = "127.0.0.1:29000"
-logfile = "/var/log/gunicorn.log"
-loglevel = "debug"
-workers = 3
-
---
+++ gunicorn_start.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+NAME="hello_app"                                  # Name of the application
+DJANGODIR=/webapps/hello_django/hello             # Django project directory
+SOCKFILE=/webapps/hello_django/run/gunicorn.sock  # we will communicte using this unix socket
+USER=hello                                        # the user to run as
+GROUP=webapps                                     # the group to run as
+NUM_WORKERS=3                                     # how many worker processes should Gunicorn spawn
+DJANGO_SETTINGS_MODULE=hello.settings             # which settings file should Django use
+DJANGO_WSGI_MODULE=hello.wsgi                     # WSGI module name
+
+echo "Starting $NAME as `whoami`"
+
+# Activate the virtual environment
+cd $DJANGODIR
+source ../bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+# Create the run directory if it doesn't exist
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+exec ../bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --user=$USER --group=$GROUP \
+  --bind=unix:$SOCKFILE \
+  --log-level=debug \
+  --log-file=-
+
