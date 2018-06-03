3a3dc5a8dd7f9916174b5817b3c4e709c122ac21
Jan Paricka <jparicka@gmail.com>
2018-06-03 23:08:39 +0200

    fixed up storage address

--- contracts/notary.sol
+++ contracts/notary.sol
@@ -6,7 +6,7 @@ contract Notary {
     address public jan = 0x45f5c8b556c3f2887b50184c823d1223f41a4156;
     address public investor = 0x33c42ED630e29B939EB9cAc3B8AbfA1a711EA1f0;
 
-    address NotaryPersistentStorageAddress = 0xb7deffae2662d7743cbe3f5c6e5904814cf2aa13;
+    address NotaryPersistentStorageAddress = 0x8439dacB099826eba3c56A8B2d3A15F108a89552;
 
     event LogResponse(bytes32, bool);
 
