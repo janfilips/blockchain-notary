c76ec609bc20f22f153b952f755b0e93df330344
Jan Filips <janfilipsgt@gmail.com>
2025-04-23 17:48:02 +0300

    Refactor database settings to use SQLite and comment out MySQL configuration

--- notary/settings-example.py
+++ notary/settings-example.py
@@ -92,23 +92,23 @@ WSGI_APPLICATION = 'notary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydatabase',
-        'USER': 'username',
-        'PASSWORD': 'your_password',
-        'HOST': '',
-        'PORT': '',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'mydatabase',
+#         'USER': 'username',
+#         'PASSWORD': 'your_password',
+#         'HOST': '',
+#         'PORT': '',
+#     }
+# }
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
